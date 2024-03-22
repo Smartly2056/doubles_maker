bd9f73be4bd4c9bc4f62a77447618a1bd31e3517
Smartly2056 <akira.tm905@gmail.com>
2024-03-23 00:47:45 +0900

    system complete

--- js/main.js
+++ js/main.js
@@ -1,56 +1,181 @@
 'use strict';
 
-const courtNumValue = 3;
-const playerNumValue = 13;
-const section = document.querySelector('section');
+const pairingForm = document.getElementById('paringForm');
+const pairingSection = document.getElementById('pairing');
+const courtList = document.getElementsByName('court_num');
+const playerNumSelect = document.getElementById('playerNumSelect');
+const courtNumSelect = document.getElementById('courtNumSelect');
+const court_len = courtList.length;
+let player_len;
+let playerList;
+let courtNum = -1;
+let playerNum = -1;
+let playerNumIndex;
+
+const create = document.getElementById('create');
+const reset = document.getElementById('reset');
 
 
-for (let i = 0; i < 5; i++) {
-    let source = [];
-    for (let j = 0; j < playerNumValue; j++) {
-        source[j] = j + 1;
+console.log(courtNum);
+console.log(playerNum);
+
+
+const playerOptions = [
+    [5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+    [8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    [12, 13, 14, 15, 16, 17, 18, 19, 20, 21],
+    [16, 17, 18, 19, 20, 21, 22, 23, 24, 25],
+];
+
+courtNumSelect.addEventListener('change', () => {
+    // 一度ラジオボタンのチェックを外す
+    if (courtNum > 0) {
+        courtList.item(courtNum - 1).checked = false;
     }
 
-    let combination = [];
-    for (let j = 0; j < playerNumValue; j++) {
-        combination[j] = source.splice(Math.floor(Math.random() * (source.length)), 1)[0];
+    // 人数の選択肢を削除
+    while (playerNumSelect.firstChild) {
+        playerNumSelect.removeChild(playerNumSelect.firstChild);
     }
 
-    let game = document.createElement('div');
-    game.classList.add('game');
+    courtNum = countCourt(courtList, court_len);
+    showPlayerOptions(courtNum);
 
-    let gameName = document.createElement('h3');
-    gameName.textContent = i + 1;
-    game.appendChild(gameName);
+    // 人数の選択肢リストの定義
+    playerList = document.getElementsByName('player_num');
+    player_len = playerList.length;
 
+});
 
-    for (let j = 0; j < courtNumValue; j++) {
-        let div = document.createElement('div');
+playerNumSelect.addEventListener('change', () => {
 
-        for (let k = 0; k < 4; k++) {
-            let span = document.createElement('span');
-            span.textContent = combination[j * 4 + k];
-            span.classList.add('player');
-            div.appendChild(span);
+    // playerNumIndex = getPlayerNumIndex(playerList, player_len)
+    playerNum = countPlayers(playerList, player_len);
+    create.disabled = false;
 
-            if (k == 1) {
-                let vsSpan = document.createElement('span');
-                vsSpan.textContent = 'vs';
-                div.appendChild(vsSpan);
-            }
+});
 
 
+create.addEventListener('click', () => {
+    generateParings(courtNum, playerNum);
+    create.disabled = true;
+    reset.disabled = false;
+});
+
+reset.addEventListener('click', () => {
+    window.location.reload();
+});
+
+
+
+// 人数選択肢の表示
+function showPlayerOptions(courtNum) {
+
+    // 質問の表示
+    const legend = document.createElement('legend');
+    legend.textContent = '人数は？';
+    playerNumSelect.appendChild(legend);
+
+    // 選択肢の作成
+    const options = playerOptions[courtNum - 1];
+
+    options.forEach((option) => {
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+
+        input.type = 'radio';
+        input.name = 'player_num';
+        input.id = 'player' + option;
+        label.appendChild(input);
+        label.appendChild(document.createTextNode(' ' + option + '人'));
+        playerNumSelect.appendChild(label);
+    });
+}
+
+// コート数を取得
+function countCourt(courtList, court_len) {
+    for (let i = 0; i < court_len; i++) {
+        if (courtList.item(i).checked) {
+            const courtNum = parseInt(courtList.item(i).id.match(/[0-9]+/g));
+            return courtNum;
+        }
+    }
+}
+
+
+// 人数を取得
+function countPlayers(playerList, player_len) {
+    for (let i = 0; i < player_len; i++) {
+        if (playerList.item(i).checked) {
+            const playerNum = parseInt(playerList.item(i).id.match(/[0-9]+/g));
+            return playerNum;
+        }
+    }
+}
+
+// 人数選択肢のindexを取得
+// playerNumIndex = getPlayerNumIndex(playerList, player_len)
+function getPlayerNumIndex(playerList, player_len) {
+    for (let i = 0; i < player_len; i++) {
+        if (playerList.item(i).checked) {
+            const playerNumIndex = i;
+            return playerNumIndex;
+        }
+    }
+}
+
+
+// ゲーム表作成
+function generateParings(courtNumValue, playerNumValue) {
+    const section = document.getElementById('paring');
+
+    for (let i = 0; i < 20; i++) {
+        let source = [];
+        for (let j = 0; j < playerNumValue; j++) {
+            source[j] = j + 1;
         }
 
-        game.appendChild(div);
+        let combination = [];
+        for (let j = 0; j < playerNumValue; j++) {
+            combination[j] = source.splice(Math.floor(Math.random() * (source.length)), 1)[0];
+        }
+
+        let game = document.createElement('div');
+        game.classList.add('game');
+
+        let gameName = document.createElement('h3');
+        gameName.textContent = i + 1;
+        game.appendChild(gameName);
+
+
+        for (let j = 0; j < courtNumValue; j++) {
+            let div = document.createElement('div');
+
+            for (let k = 0; k < 4; k++) {
+                let span = document.createElement('span');
+                span.textContent = combination[j * 4 + k];
+                span.classList.add('player');
+                div.appendChild(span);
+
+                if (k == 1) {
+                    let vsSpan = document.createElement('span');
+                    vsSpan.textContent = 'vs';
+                    div.appendChild(vsSpan);
+                }
+            }
+
+            game.appendChild(div);
+        }
+
+        section.appendChild(game);
+
+        console.log(combination);
+        console.log(section);
+
     }
-
-    section.appendChild(game);
-
-    console.log(combination);
-    console.log(section);
-
 }
+
+
 
 
 
