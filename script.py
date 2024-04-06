855bb9675038f558ff9187a84489bcaee5ad4ee0
Smartly2056 <akira.tm905@gmail.com>
2024-04-07 08:43:45 +0900

    add scraping hist

--- script.py
+++ script.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 driver = webdriver.Chrome()
 
@@ -19,48 +22,56 @@ if radio_court.is_enabled():
 else:
     print('nothing selected!')
     
-time.sleep(1.5)
+time.sleep(1)
 
 # 人数選択
 try:
     radio_player = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "player12")))
     radio_player.click()
-    # print('player selected!')
 except:
     print('nothing selected!')
     
-time.sleep(1.5)
-
+time.sleep(1)
+    
+    
 # ゲーム表作成
 try:
     create = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "create")))
     create.click()
-    # print('pairing created normally!')
 except:
     print('nothing created!')
     
-time.sleep(1.5)
+time.sleep(1)
+
 
 # 待機者取得
+All_waiting = []
 waitings = driver.find_elements(By.CLASS_NAME, "waiting")
 for waiting in waitings:
     ul = waiting.find_element(By.TAG_NAME, "ul")
     lis = ul.find_elements(By.TAG_NAME, "li")
     
     for li in lis:        
-        print(li.text, end=',')
+        All_waiting.append(int(li.text))
     
-    print('\n')
-    time.sleep(1.5)
+    time.sleep(1)
+
+print(All_waiting)
+
+# 人数計算
+for i in range(12):
+    print(f'{i+1}: {All_waiting.count(i + 1)}')
+
+time.sleep(1)
 
 
-# h1取得
-h1_element = driver.find_element(by=By.TAG_NAME, value="h1")
-h1_text = h1_element.text
-
-driver.implicitly_wait(5)
+# ヒストグラム作成
+plt.hist(All_waiting, bins=12)
+plt.show()
 
 
+
+driver.implicitly_wait(3)
 # スクレイピング終了
 driver.quit()
 
